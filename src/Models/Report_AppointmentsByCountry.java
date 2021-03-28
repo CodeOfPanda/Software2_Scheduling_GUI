dae9cde611894d5dd4678fe2bfa3de04f0ae6110
CodeOfPanda <mlynnleigland@gmail.com>
2021-03-27 22:30:04 -0600

    finished javadoc for reports models

--- src/Models/Report_AppointmentsByCountry.java
+++ src/Models/Report_AppointmentsByCountry.java
@@ -1,22 +1,31 @@
 package Models;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
+/** This class creates Report_AppointmentsByCountry and is responsible for holding Report_AppointmentsByCountry data for retrieval.*/
 public class Report_AppointmentsByCountry {
     private String country;
     private String type;
     private int count;
 
+/** This is a constructor method that creates instances of the Report_AppointmentsByCountry model.
+ *  This method creates instances of the Report_AppointmentsByCountry model.
+ *  @param country Country Name
+ *  @param type Appointment Type
+ *  @param count number of appointments with specific type*/
     public Report_AppointmentsByCountry(String country, String type, int count) {
         this.country = country;
         this.type = type;
         this.count = count;
     }
-
+    /** This method holds the Country.
+     *  This method is a getter method that holds the Country's name value and can pass that value when called.
+     *  @return Returns the Country*/
     public String getReportApptCountry() {return country;}
+    /** This method holds the appointment Type.
+     *  This method is a getter method that holds the appointment Type value and can pass that value when called.
+     *  @return Returns the appointment Type*/
     public String getReportApptType() {return type;}
+    /** This method holds the number of appointments with specific type.
+     *  This method is a getter method that holds the number of appointments with a specific types value and can pass that value when called.
+     *  @return Returns the number of appointments with specific type*/
     public int getReportApptCount() {return count;}
 }
--- src/Models/Report_CustomerAppointments.java
+++ src/Models/Report_CustomerAppointments.java
@@ -1,23 +1,33 @@
 package Models;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
+/** This class creates Report_CustomerAppointments and is responsible for holding Report_CustomerAppointments data for retrieval.*/
 public class Report_CustomerAppointments {
     private String month;
     private int count;
     private String type;
 
+    /** This is a constructor method that creates instances of the Report_CustomerAppointments model.
+     *  This method creates instances of the Report_CustomerAppointments model.
+     *  @param month The current month
+     *  @param type Appointment Type
+     *  @param count number of appointments with specific type*/
     public Report_CustomerAppointments (String month, int count, String type) {
         this.month = month;
         this.count = count;
         this.type = type;
     }
 
+    /** This method holds the current month.
+     *  This method is a getter method that holds the current month value and can pass that value when called.
+     *  @return Returns the current month*/
     public String getReportCustMonth() {return month;}
+    /** This method holds the number of appointments with specific type.
+     *  This method is a getter method that holds the number of appointments with a specific types value and can pass that value when called.
+     *  @return Returns the number of appointments with specific type*/
     public int getReportCustCount() {return count;}
+    /** This method holds the appointment Type.
+     *  This method is a getter method that holds the appointment Type value and can pass that value when called.
+     *  @return Returns the appointment Type*/
     public String getReportType() {return type;}
 
 }
