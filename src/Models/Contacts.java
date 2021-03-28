7f5e0911ee4f44ba416a1d17f45b1c0d84100505
CodeOfPanda <mlynnleigland@gmail.com>
2021-03-27 20:46:45 -0600

    finished javadoc for Contact's model

--- src/Models/Contacts.java
+++ src/Models/Contacts.java
@@ -2,35 +2,27 @@ package Models;
 
 import javafx.beans.property.*;
 
+/** This class creates Contacts and is responsible for holding Contact data for retrieval.*/
 public class Contacts {
-    private IntegerProperty contactID;
+    private int contactID;
     private StringProperty contactName;
-    private StringProperty contactEmail;
-    private static int ctID;
-    private static String ctName;
+    private String contactEmail;
 
+    /** This is a constructor method that creates instances of the Contacts Model.
+     *  This method creates instances of the Contacts model.
+     *  @param contactID The Contact's ID
+     *  @param name The Contact's Name
+     *  @param email The Contact's Email*/
     public Contacts(int contactID, String name, String email) {
-        this.contactID = new SimpleIntegerProperty(contactID);
-        ctID = contactID;
+        this.contactID = contactID;
         contactName = new SimpleStringProperty(name);
-        contactEmail = new SimpleStringProperty(email);
+        contactEmail = email;
     }
 
-    public static int getContactID() {
-        return ctID;
-    }
-    // contact name
-    public StringProperty getContactName() {
-        return contactName;
-    }
+    /** This method holds the Contact Name.
+     *  This method is a getter method that holds the Contact Name value and can pass that value when called.
+     *  @return Returns Contact Name*/
     public String getName(){return contactName.get();}
 
-    // contact email
-    public StringProperty getContactEmail() {
-        return contactEmail;
-    }
 }
 
-//Contact_ID int(10) AI PK
-//Contact_Name varchar(50)
-//Email varchar
